a197b66f1870c2461bf189505510740b0c5a4d74
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2025-02-14 14:49:46 -0300

    a
    a

---
+++ ArquivoX/ImgToText Desktop Version/ImgToText Desktop Version/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ImgToText_Desktop_Version
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            richTextBox1 = new RichTextBox();
+            textBox1 = new TextBox();
+            openFileDialog1 = new OpenFileDialog();
+            richTextBox2 = new RichTextBox();
+            richTextBox3 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(353, 303);
+            button1.Name = "button1";
+            button1.Size = new Size(429, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Converter todas imagens em texto (Github)";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.ButtonFace;
+            button2.Location = new Point(12, 63);
+            button2.Name = "button2";
+            button2.Size = new Size(306, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Abrir IMG";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.Location = new Point(12, 92);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(306, 141);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(306, 16);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "Senha";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.BorderStyle = BorderStyle.None;
+            richTextBox2.Location = new Point(324, 92);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(306, 141);
+            richTextBox2.TabIndex = 4;
+            richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.BorderStyle = BorderStyle.None;
+            richTextBox3.Location = new Point(12, 239);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(306, 141);
+            richTextBox3.TabIndex = 5;
+            richTextBox3.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox3);
+            Controls.Add(richTextBox2);
+            Controls.Add(textBox1);
+            Controls.Add(richTextBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private RichTextBox richTextBox1;
+        private TextBox textBox1;
+        private OpenFileDialog openFileDialog1;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox3;
+    }
+}
