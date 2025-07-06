b365740192c40febf9680132a83ad89b56c4b8d6
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2025-07-06 15:02:02 -0300

    Commit autom├ítico em 06/07/2025 ├ás 15:02:01,92

--- Documentos/service-worker.js
+++ Documentos/service-worker.js
@@ -5,29 +5,35 @@ const urlsToCache = [
   '181534.png'
 ];
 
-// Instala e faz cache
+ //Registre os arquivos no evento install:
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
   );
 });
 
 // Ativa e limpa caches antigos
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key)))
-    )
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
 // Responde com cache ou fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
